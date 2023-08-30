a2274bbad8b90786783101c25dd39a03c952a474
Thinktan <tantao0675@hnu.edu.cn>
2023-08-31 01:47:24 +0800

    commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # 这是一个示例 Python 脚本。
-import torch
 
 # 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
@@ -7,8 +6,8 @@ import torch
 import torch
 
 print(torch.cuda.is_available())
-x = torch.rand(5,3)
-print(x)
+# x = torch.rand(5,3)
+# print(x)
 
 def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
