7d47ab29bcb775f06dc0dbdf783e824abd60e9ac
Thinktan <tantao0675@hnu.edu.cn>
2023-09-27 11:41:04 +0800

    up from win

--- SSD/ssd300.py
+++ SSD/ssd300.py
@@ -68,7 +68,44 @@ class VGG16Extractor300(nn.Module):
 class SSD300(nn.Module):
     steps = (8, 16, 32, 64, 100, 300) # 锚点框的步长
     box_sizes = (30, 60, 111, 162, 213, 264, 315) # 锚点框的基础大小
-    
+    aspect_ratios = ((2,), (2, 3), (2, 3), (2, 3), (2,), (2,)) # 锚点框的宽高比
+    fm_sizes = (38, 19, 10, 5, 3, 1) # 分类的个数
+
+    def __init__(self, num_classes):
+        super(SSD300, self).__init__()
+        self.num_classes = num_classes
+        self.num_anchors = (4, 6, 6, 6, 4, 4)
+        self.in_channels = (512, 1024, 512, 256, 256, 256)
+
+        self.extractor = VGG16Extractor300()
+        self.loc_layers = nn.ModuleList() # 选框位置的预测层
+        self.cls_layers = nn.ModuleList() # 选框所包含物体种类的预测层
+
+        for i in range(len(self.in_channels)):
+             self.loc_layers += [nn.Conv2d(self.in_channels[i],
+                                           self.num_anchors[i]*4, kernel_size=3, padding=1)]
+             self.cls_layers += [nn.Conv2d(self.in_channels[i],
+                                           self.num_anchors[i]*self.num_classes, kernel_size=3,
+                                           padding=1)]
+
+    def forward(self, x):
+        loc_preds = []
+        cls_preds = []
+        xs = self.extractor(x) # 那个hs
+        for i, x in enumerate(xs):
+            loc_pred = self.loc_layers[i](x)
+            loc_pred = loc_pred.permute(0, 2, 3, 1).contiguous()
+            loc_preds.append(loc_pred.view(loc_pred.size(0), -1, 4))
+
+            cls_pred = self.cls_layers[i](x)
+            cls_pred = cls_pred.permute(0, 2, 3, 1).contiguous()
+            cls_preds.append(cls_pred.view(cls_pred.size(0), -1, self.num_classes))
+
+        loc_preds = torch.cat(loc_preds, 1)
+        cls_preds = torch.cat(cls_preds, 1)
+        return loc_preds, cls_preds
+
+
 
 
 
